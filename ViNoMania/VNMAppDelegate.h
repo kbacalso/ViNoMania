3c936e384b0e53b5f6d6c9b47fa55df63910c40d
Katreena Bacalso <k.bacalso@klab.com>
2012-11-27 09:44:45 +0800

    Initial Commit

---
+++ ViNoMania/VNMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  VNMAppDelegate.h
+//  ViNoMania
+//
+//  Created by Cys-iOS dev on 11/27/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VNMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
